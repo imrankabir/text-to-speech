1bdfa10dcf364c972f4bda44a68ce3e3ee428347
Imran Kabir <imran.kabir.k@gmail.com>
2024-08-01 20:00:23 +0500

    text-to-speech auto commit on 2024-08-01 08:00:23 PM

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const speakBtn = document.querySelector("#speak-btn");
 const textToSpeak = document.querySelector("#text-to-speak");
+const lang = document.querySelector("#lang");
 
 if ("speechSynthesis" in window) {
     const synthesis = window.speechSynthesis;
     const utterance = new SpeechSynthesisUtterance();
-    utterance.lang = "en-US"; // Set the language
+    // utterance.lang = "en-US"; // Set the language
     utterance.volume = 1; // Set the volume (0 to 1)
     utterance.rate = 1; // Set the rate (0.1 to 10)
     utterance.pitch = 1; // Set the pitch (0 to 2)
-    speakBtn.addEventListener("click", (event) => {
+    speakBtn.addEventListener('click', e => {
+        utterance.lang = lang.value;
         utterance.text = textToSpeak.value;
         synthesis.speak(utterance);
     });
@@ -19,6 +21,6 @@ if ("speechSynthesis" in window) {
 textToSpeak.addEventListener('click', e => {
     if (textToSpeak.value != '') {
         textToSpeak.select();
-        document.execCommand('copy');
+        // document.execCommand('copy');
     }
 });
