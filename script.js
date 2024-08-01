5b5b83552dd723b25f840d2afac9537070aa31a8
Imran Kabir <imran.kabir.k@gmail.com>
2024-08-01 19:10:17 +0500

    text-to-speech auto commit on 2024-08-01 19:10:17

---
+++ script.js
@@ -0,0 +1,24 @@
+const speakBtn = document.querySelector("#speak-btn");
+const textToSpeak = document.querySelector("#text-to-speak");
+
+if ("speechSynthesis" in window) {
+    const synthesis = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.lang = "en-US"; // Set the language
+    utterance.volume = 1; // Set the volume (0 to 1)
+    utterance.rate = 1; // Set the rate (0.1 to 10)
+    utterance.pitch = 1; // Set the pitch (0 to 2)
+    speakBtn.addEventListener("click", (event) => {
+        utterance.text = textToSpeak.value;
+        synthesis.speak(utterance);
+    });
+} else {
+    console.warn("Web Speech API is not supported in this browser.");
+}
+
+textToSpeak.addEventListener('click', e => {
+    if (textToSpeak.value != '') {
+        textToSpeak.select();
+        document.execCommand('copy');
+    }
+});
